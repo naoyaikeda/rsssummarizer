433f1f888edd4645ed749636b6896bc05f4795da
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2025-09-21 15:07:37 +0900

    キャッシュファイルの名称を正規版に戻した。

--- rsssummarizer.py
+++ rsssummarizer.py
@@ -41,7 +41,7 @@ def main(logger:logging.Logger):
 
     storage_dir = prepare_storage(profile_dir)
 
-    profile_store = TinyDB(os.path.join(storage_dir, "rsssummarizer-ex002.db"))
+    profile_store = TinyDB(os.path.join(storage_dir, "rsssummarizer.db"))
 
     if getattr(sys, 'frozen', False):
         # PyInstallerで実行されている場合、実行ファイルと同じディレクトリにあるrsssummarizer.envを読み込む
